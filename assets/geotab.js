2e318754b287ff45c2064abb080ae1678a37cbaa
amitrohan <amit@kruzr.co>
2022-02-28 01:46:53 +0530

    cl

--- assets/geotab.js
+++ assets/geotab.js
@@ -24,6 +24,7 @@ geotab.addin.driverProfile = () => {
   };
 
   let onAppStart = () => {
+    localStorage.setItem("tripData","");
     loadDriverProfileRuntime();
     loadDriverProfilePolyfill();
     loadDriverProfileMain();
